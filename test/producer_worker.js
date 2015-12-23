8fb198a59a8a881ae238dcb8f314a94f6a84e3f6
Waiter Zen <waiter.james1991@gmail.com>
2015-12-24 01:47:22 +0800

    format  producer_worker

--- test/producer_worker.js
+++ test/producer_worker.js
@@ -3,27 +3,27 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const expect = chai.expect;
-const producerWorker = require('../lib/producerWorker');
+const producerWorker = require('../lib/producer_worker');
 
 chai.use(chaiAsPromised);
 describe('test producerWorker', function () {
   this.timeout(15000);
 
-  it('should can save run producerWorker', function (done) {
+  it('should can run producerWorker, and got jobId', function (done) {
     const seed = {
       type: 'convert',
-      payload:{
+      payload: {
         from: 'HKD',
         to: 'USD'
       }
     };
-    producerWorker.putSeed(seed).then(function(jobId){
-      console.log("got jobId:", jobId);
+
+    producerWorker.putSeed(seed).then(function (jobId) {
+      console.log('got jobId:', jobId);
       expect(jobId).not.to.be.null;
       done();
-    }).catch(function(e) {
-      console.log("got error:", e);
+    }).catch(function (e) {
+      console.log('got error:', e);
     });
   });
-
 });
